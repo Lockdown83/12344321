ab0705f4bdaa444815e442e1e0f891ae43358900
Lockdown83 <andrewdimaulozx@gmail.com>
2024-11-20 22:29:47 -0800

    add and commit

---
+++ cars/ford.py
@@ -0,0 +1,13 @@
+def draw_truck():
+    truck = r"""
+        ______
+       |      |____
+       |  ____|    |
+       | |         |
+       |_|_________|
+         O       O
+    """
+    print(truck)
+
+# Call the function to draw the truck
+draw_truck()
